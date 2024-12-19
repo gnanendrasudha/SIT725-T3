9390d48d3499237de731e0824c1714ed364c2834
gnanendrasudha <s225045496@deakin.edu.au>
2024-12-19 13:18:34 +1100

    y

--- project/controllers/vaultController.js
+++ project/controllers/vaultController.js
@@ -1,21 +1,41 @@
 // vaultController.js
-
-const VaultModel = require('./vaultModel');  // Import VaultModel from vaultModel.js
-
 class VaultController {
     constructor() {
-        this.model = new VaultModel();  // Now we can use VaultModel
+        this.entries = [];
     }
 
-    handleAddEntry(entry) {
-        this.model.addEntry(entry);
-        console.log('Entry added:', entry);
+    handleAddEntry() {
+        const input = document.getElementById('entryInput');
+        const entry = input.value;
+        if (entry) {
+            this.entries.push(entry);
+            console.log('Entry added:', entry);
+            this.updateEntriesView();
+            input.value = '';  // Clear the input after adding
+        } else {
+            alert('Please enter a value.');
+        }
     }
 
-    handleGetEntries() {
-        console.log(this.model.getEntries());
+    updateEntriesView() {
+        const entriesContainer = document.getElementById('entriesContainer');
+        entriesContainer.innerHTML = '';
+        this.entries.forEach(entry => {
+            const entryDiv = document.createElement('div');
+            entryDiv.textContent = entry;
+            entriesContainer.appendChild(entryDiv);
+        });
     }
 }
 
+// Export the VaultController to be used in app.js
 module.exports = VaultController;
+
+
+function calculateStrength(value1, value2) {
+    if (value1 < 0 || value2 < 0) {
+        throw new Error('Invalid input: Values must be non-negative');
+    }
+    return value1 + value2;
+}
 
